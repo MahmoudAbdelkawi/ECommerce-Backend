f506b73d825aad6ee33968600ff0ca57243b5962
Yussif Muhammed <yussifmohd17@gmail.com>
2024-05-12 22:40:40 +0300

    Prepare Solution

--- ECommerce.Api/obj/Debug/net8.0/ECommerce.Api.AssemblyInfo.cs
+++ ECommerce.Api/obj/Debug/net8.0/ECommerce.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6908be2909bb0080d37222151c06266bec94d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
